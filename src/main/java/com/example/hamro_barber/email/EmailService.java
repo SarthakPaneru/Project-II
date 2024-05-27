95aacfa94ca282cab580bb2752a68b1cd57e90c4
Suman Sapkota <suman2020j@gmail.com>
2024-05-27 20:56:12 +0545

    Dependency updated/pom file

--- src/main/java/com/example/hamro_barber/email/EmailService.java
+++ src/main/java/com/example/hamro_barber/email/EmailService.java
@@ -32,6 +32,7 @@ public class EmailService implements EmailSender{
     private final static Logger LOGGER = LoggerFactory
             .getLogger(EmailService.class);
 
+
     private final JavaMailSender mailSender;
     private final ConfirmationTokenService confirmationTokenService;
 
