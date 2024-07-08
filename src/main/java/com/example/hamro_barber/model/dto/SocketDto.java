ad1d34ae749b6682679b8a76e5154815f4264d88
Sarthak <sarthak.paneru007@gmail.com>
2024-07-08 23:28:36 +0545

    fix: socket message error resolved

--- src/main/java/com/example/hamro_barber/model/dto/SocketDto.java
+++ src/main/java/com/example/hamro_barber/model/dto/SocketDto.java
@@ -1,10 +1,8 @@
 package com.example.hamro_barber.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
